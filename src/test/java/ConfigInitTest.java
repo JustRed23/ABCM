fcea4b6fe10bdb0485d30993d5d5f43315f7e537
JustRed23 <manuel.bormans@gmail.com>
2022-11-27 01:17:02 +0100

    Add tests

---
+++ src/test/java/ConfigInitTest.java
@@ -0,0 +1,31 @@
+import dev.JustRed23.abcm.Config;
+import dev.JustRed23.abcm.exception.ConfigAlreadyInitializedException;
+import dev.JustRed23.abcm.exception.ConfigInitException;
+import dev.JustRed23.abcm.exception.ConfigNotInitializedException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ConfigInitTest {
+
+    @Test
+    void testConfig() {
+        System.out.println("TEST 1");
+        Config.setDebug(true);
+        assertDoesNotThrow(Config::init);
+        Config.destroy();
+    }
+
+    @Test
+    void testThrows() throws ConfigInitException {
+        System.out.println("TEST 2");
+        assertThrows(ConfigNotInitializedException.class, () -> Config.rescan(false));
+        Config.setDebug(true);
+        Config.init();
+        assertThrows(ConfigAlreadyInitializedException.class, Config::init);
+        assertThrows(ConfigAlreadyInitializedException.class, () -> Config.setDebug(false));
+        assertDoesNotThrow(() -> Config.rescan(true));
+        Config.destroy();
+    }
+}
